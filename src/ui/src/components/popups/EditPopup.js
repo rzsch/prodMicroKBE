4449554663861464a02592a2740037f5967746c1
rzsch <r.zschaeckel@gmail.com>
2023-08-04 17:09:46 +0200

    Added functionality to edit and delete products if you are the seller of the product or admin.

--- src/ui/src/components/popups/EditPopup.js
+++ src/ui/src/components/popups/EditPopup.js
@@ -1,4 +1,4 @@
-import './LoginPopup.css'
+import './EditPopup.css'
 import {useContext} from 'react';
 import PopupContext from '../contexts/PopupContext.js'
 import ProductContext from '../contexts/ProductContext.js'
@@ -11,25 +11,39 @@ function EditPopup(props) {
     const idOfProductToEdit = props.idOfProductToEdit
     const indexInProductList = products.findIndex(product => product.id === idOfProductToEdit)
 
-    /*let name = products[indexInProductList].name
-    let price = products[indexInProductList].price
-    let brand = products[indexInProductList].brand
-    let size = products[indexInProductList].size
-    let hdmi = products[indexInProductList].hdmi
-    let dp = products[indexInProductList].dp
-    let vga = products[indexInProductList].vga
-    let dvi = products[indexInProductList].dvi
-    let usb = products[indexInProductList].usb
-    let aux = products[indexInProductList].aux
-    let link = products[indexInProductList].link
-    let seller = products[indexInProductList].seller*/
+    let product = products[indexInProductList]
+    const seller = product.seller
+
+    function generateEntry(eventTarget) {
+        let entry = {}
+        entry.id = idOfProductToEdit
+        entry.name = eventTarget.name.value
+        entry.price = parseInt(eventTarget.price.value)
+        entry.brand = eventTarget.brand.value
+        entry.size = parseFloat(eventTarget.size.value)
+        entry.hdmi = parseInt(eventTarget.hdmi.value)
+        entry.dp = parseInt(eventTarget.dp.value)
+        entry.vga = parseInt(eventTarget.vga.value)
+        entry.dvi = parseInt(eventTarget.dvi.value)
+        entry.usb = parseInt(eventTarget.usb.value)
+        entry.aux = parseInt(eventTarget.aux.value)
+        entry.link = eventTarget.link.value
+        entry.seller = seller
+        return entry
+    }
 
     const edit = (event) => {
         event.preventDefault()
+        const newEntry = generateEntry(event.target)
         const newProductList = products.slice(0)
-        newProductList[indexInProductList].price = 123
+        newProductList[indexInProductList] = newEntry
         setProducts(newProductList)
         setPopupPlaceholder(null)
+    }
+
+    function removeFromProducts() {
+        setPopupPlaceholder(null)
+        setProducts(products.filter(product => product.id !== idOfProductToEdit))
 
     }
 
@@ -48,13 +62,49 @@ function EditPopup(props) {
                 </div>
                 <div className="form-area">
                     <form onSubmit={edit}>
-                        <label htmlFor="username">Username:</label><br/>
-                        <input className="login-input" type="text" id="username" name="username" placeholder="Username"/><br/><br/>
-                        <label htmlFor="password">Password:</label><br/>
-                        <input className="login-input" type="password" id="password" name="password" placeholder="Password"/><br/><br/>
+                        <label htmlFor="name">Name:</label><br/>
+                        <input className="edit-input" type="text" id="name" name="name" defaultValue={product.name}/><br/>
+                        <label htmlFor="price">Price:</label><br/>
+                        <input className="edit-input" type="number" id="price" name="price" min="0" max="99999" defaultValue={product.price}/><br/>
+                        <label htmlFor="brand">Brand:</label><br/>
+                        <input className="edit-input" type="text" id="brand" name="brand" defaultValue={product.brand}/><br/>
+                        <label htmlFor="size">Size:</label><br/>
+                        <input className="edit-input" type="number" id="size" name="size" min="0" step ="0.01" defaultValue={product.size}/><br/>
+                        Ports:
+                        <div className="edit-ports">
+                            <div>
+                                <label htmlFor="hdmi">HDMI:</label><br/>
+                                <input className="edit-port" type="number" id="hdmi" name="hdmi" min="0" max="9" defaultValue={product.hdmi}/><br/>
+                            </div>
+                            <div>
+                                <label htmlFor="dp">DP:</label><br/>
+                                <input className="edit-port" type="number" id="dp" name="dp" min="0" max="9" defaultValue={product.dp}/><br/>
+                            </div>
+                            <div>
+                                <label htmlFor="vga">VGA:</label><br/>
+                                <input className="edit-port" type="number" id="vga" name="vga" min="0" max="9" defaultValue={product.vga}/><br/>
+                            </div>
+                            <div>
+                                <label htmlFor="dvi">DVI:</label><br/>
+                                <input className="edit-port" type="number" id="dvi" name="dvi" min="0" max="9" defaultValue={product.dvi}/><br/>
+                            </div>
+                            <div>
+                                <label htmlFor="usb">USB:</label><br/>
+                                <input className="edit-port" type="number" id="usb" name="usb" min="0" max="9" defaultValue={product.usb}/><br/>
+                            </div>
+                            <div>
+                                <label htmlFor="aux">AUX:</label><br/>
+                                <input className="edit-port" type="number" id="aux" name="aux" min="0" max="9" defaultValue={product.aux}/><br/>
+                            </div>
+                        </div>
+                        <label htmlFor="link">link:</label><br/>
+                        <textarea className="edit-input" id="link" name="link" rows="5" defaultValue={product.link}/><br/>
                         <input className="submit-button" type="submit" value="Edit"/>
                     </form>
                 </div>
+                <button className="delete-button" onClick={removeFromProducts}>
+                    Delete
+                </button>
             </div>
         </div>
     );
